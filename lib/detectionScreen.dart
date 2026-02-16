d69a6186349b17fa3fd2334802e8aac971fc17bd
IK <ingkhing05@gmail.com>
2026-02-16 15:35:07 +0800

    Remove unused dart:typed_data import in detectionScreen.dart

--- lib/detectionScreen.dart
+++ lib/detectionScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:google_fonts/google_fonts.dart';
